97000b73e577b32c46c669b5b6be08bf2fa69524
cmlee <chukmunnlee@gmail.com>
2026-06-13 07:23:55 +0800

    video 03

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import {Hello} from './hello/hello';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Hello],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
