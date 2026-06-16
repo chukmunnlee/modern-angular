8ecf90e592f8886c0b7b231d3a6c7ba9f168f9a5
Lee Chuk Munn <chukmunnlee@gmail.com>
2026-06-17 07:48:05 +0800

    video 14

--- src/app/ui/products/products-grid/products-grid.ts
+++ src/app/ui/products/products-grid/products-grid.ts
@@ -1,14 +1,33 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import {ProductCard} from '../product-card/product-card';
 import {Product, SAMPLE_PRODUCTS} from '../product';
-import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-products-grid',
-  imports: [ ProductCard, MatIconModule ],
+  imports: [ ProductCard, MatIconModule, MatInputModule, MatFormFieldModule, FormsModule ],
   templateUrl: './products-grid.html',
   styleUrl: './products-grid.scss',
 })
 export class ProductsGrid {
   protected readonly products = signal<Product[]>([...SAMPLE_PRODUCTS])
+  protected readonly searchTerm = signal<string>('');
+  protected readonly filteredProducts = computed(() => {
+    const term = this.searchTerm().toLowerCase()
+    if (!term)
+      return this.products()
+    return this.products().filter(prod => prod.name.toLowerCase().includes(term)
+        || prod.description.toLowerCase().includes(term))
+  })
+
+  protected clearSearch(): void {
+    this.searchTerm.set('')
+  }
+
+  protected trimSearch(): void {
+    this.searchTerm.update((val: string) => val.trim())
+  }
 }
