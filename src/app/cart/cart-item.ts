dc89b9d071cac2bd91d3ae6e287e61b5dd327362
Lee Chuk Munn <chukmunnlee@gmail.com>
2026-06-23 07:34:19 +0800

    add to cart

---
+++ src/app/cart/cart-item.ts
@@ -0,0 +1,6 @@
+import {Product} from "../ui/products/product";
+
+export interface CartItem {
+  product: Product
+  quantity: number
+}
--- src/app/cart/cart-service.ts
+++ src/app/cart/cart-service.ts
@@ -1,14 +1,25 @@
-import { Injectable, signal } from '@angular/core';
+import { computed, Injectable, signal } from '@angular/core';
 import {Product} from '../ui/products/product';
+import {CartItem} from './cart-item';
 
 @Injectable({ providedIn: 'root' })
 export class CartService {
 
-  private readonly contents = signal<Product[]>([])
+  private readonly contents = signal<CartItem[]>([])
+  readonly totalItems = computed(() => this.contents().reduce((acc, curr) => acc + curr.quantity, 0))
 
-  addToCart(prod: Product) {
+  addToCart(product: Product) {
     // Need to create a new contents, do not mutate existing object
-    console.info(`Add product ${prod.name} to cart`)
-    this.contents.update(contents => [ ...contents, prod ])
+    console.info(`Add product ${product.name} to cart`)
+    this.contents.update(items => {
+      const idx = items.findIndex(cartItem => cartItem.product.id == product.id)
+      if (idx < 0)
+        return [ ...items, { product, quantity: 1 } ]
+
+      items[idx] = { ...items[idx], quantity: items[idx].quantity + 1 }
+      return [ ...items ]
+
+    })
+    //this.contents.update(contents => [ ...contents, prod ])
   }
 }
